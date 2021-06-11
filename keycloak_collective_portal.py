82a506278bcbd7cda98354b283953e5611b0b4e0
decentral1se <hi@decentral1.se>
2021-06-11 14:23:13 +0200

    Basic login redirect

--- keycloak_collective_portal.py
+++ keycloak_collective_portal.py
@@ -1,15 +1,27 @@
 """Community Keycloak SSO user management."""
 
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="mysecretzkey")
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    user = request.session.get("user")
+    if user:
+        return templates.TemplateResponse(
+            "index.html", context={"request": request}
+        )
+    return RedirectResponse("/login")
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
     return templates.TemplateResponse(
-        "index.html", context={"request": request}
+        "login.html", context={"request": request}
     )
