043eaff5fde607044474974533d85a5738bf8546
decentral1se <hi@decentral1.se>
2021-06-11 14:14:04 +0200

    Templates support

--- keycloak_collective_portal.py
+++ keycloak_collective_portal.py
@@ -1,10 +1,15 @@
 """Community Keycloak SSO user management."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html", context={"request": request}
+    )
