80dd93823e7ca62e3e7fa0b1b795c6b9f5254575
decentral1se <hi@decentral1.se>
2021-06-12 19:46:48 +0200

    Invite links prototype working

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # keycloak-collective-portal
 
+> **WARNING**: this software is in a pre-alpha quality state and is an initial
+> prototype. It is being developed within the context of
+> [lumbung.space](https://lumbung.space/) and maybe have hard-coded values and
+> configuration specifically for that environment. If the idea of this software
+> sounds interesting to you, please let us know on the issue tracker!
+
 [![Build Status](https://drone.autonomic.zone/api/badges/autonomic-cooperative/keycloak-collective-portal/status.svg?ref=refs/heads/main)](https://drone.autonomic.zone/autonomic-cooperative/keycloak-collective-portal)
 
 > Community Keycloak SSO user management
@@ -42,9 +48,12 @@ your technology stack.
 
 ## Hacking
 
-It's a [FastAPI](https://fastapi.tiangolo.com/) application.
+It's a [FastAPI](https://fastapi.tiangolo.com/) application. Currently being
+developed with Python 3.9. Once we move out of the prototype stage, more
+version compatability will be offered.
 
 ```
+$ docker run -p 6379:6379 -d redis:6-alpine
 $ set -a && source .envrc && set +a
 $ make
 ```
--- keycloak_collective_portal.py
+++ keycloak_collective_portal.py
@@ -9,10 +9,11 @@ from uuid import uuid4
 import httpx
 from aioredis import create_redis_pool
 from authlib.integrations.starlette_client import OAuth, OAuthError
-from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, Form, HTTPException, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from humanize import naturaldelta
+from keycloak import KeycloakAdmin
 from starlette.exceptions import HTTPException
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -34,17 +35,6 @@ INVITE_TIME_LIMIT = environ.get("INVITE_TIME_LIMIT")
 app = FastAPI(docs_url=None, redoc_url=None)
 app.add_middleware(SessionMiddleware, secret_key=APP_SECRET_KEY)
 templates = Jinja2Templates(directory="templates")
-
-oauth = OAuth()
-oauth.register(
-    name="keycloak",
-    client_kwargs={"scope": "openid profile email"},
-    client_id=KEYCLOAK_CLIENT_ID,
-    client_secret=KEYCLOAK_CLIENT_SECRET,
-    authorize_url=f"{BASE_URL}/auth",
-    access_token_url=f"{BASE_URL}/token",
-    jwks_uri=f"{BASE_URL}/certs",
-)
 
 
 class RequiresLoginException(Exception):
@@ -74,8 +64,18 @@ async def get_user(request: Request):
 
 
 async def get_invites(request: Request, user=Depends(get_user)):
+    if not user:
+        idx, invites = b"0", {}
+        while idx:
+            idx, username = await app.state.redis.scan(idx)
+            invites[username[0]] = json.loads(
+                await app.state.redis.get(username[0])
+            )
+        return invites
+
     username = user["preferred_username"]
     invites = await app.state.redis.get(username)
+
     if invites:
         humanised = []
         for invite in json.loads(invites):
@@ -85,6 +85,7 @@ async def get_invites(request: Request, user=Depends(get_user)):
             )
             humanised.append(invite)
         return humanised
+
     return []
 
 
@@ -92,6 +93,25 @@ async def get_invites(request: Request, user=Depends(get_user)):
 async def starup_event():
     app.state.redis = await create_redis_pool(
         f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}?encoding=utf-8"
+    )
+
+    oauth = OAuth()
+    oauth.register(
+        name="keycloak",
+        client_kwargs={"scope": "openid profile email"},
+        client_id=KEYCLOAK_CLIENT_ID,
+        client_secret=KEYCLOAK_CLIENT_SECRET,
+        authorize_url=f"{BASE_URL}/auth",
+        access_token_url=f"{BASE_URL}/token",
+        jwks_uri=f"{BASE_URL}/certs",
+    )
+    app.state.oauth = oauth
+
+    app.state.keycloak = KeycloakAdmin(
+        server_url=f"https://{KEYCLOAK_DOMAIN}/auth/",
+        realm_name=KEYCLOAK_REALM,
+        client_secret_key=KEYCLOAK_CLIENT_SECRET,
+        verify=True,
     )
 
 
@@ -119,17 +139,19 @@ async def login(request: Request):
 @app.get("/login/keycloak")
 async def login_keycloak(request: Request):
     redirect_uri = request.url_for("auth_keycloak")
-    return await oauth.keycloak.authorize_redirect(request, redirect_uri)
+    return await app.state.oauth.keycloak.authorize_redirect(
+        request, redirect_uri
+    )
 
 
 @app.get("/auth/keycloak")
 async def auth_keycloak(request: Request):
     try:
-        token = await oauth.keycloak.authorize_access_token(request)
+        token = await app.state.oauth.keycloak.authorize_access_token(request)
     except Exception as exc:
         return HTMLResponse(f"<p>{exc} (<a href='{home}'>home</a>)</p>")
 
-    user = await oauth.keycloak.parse_id_token(request, token)
+    user = await app.state.oauth.keycloak.parse_id_token(request, token)
     request.session["user"] = dict(user)
 
     return RedirectResponse(request.url_for("home"))
@@ -152,7 +174,8 @@ async def invite_keycloak_create(
     request: Request, user=Depends(get_user), invites=Depends(get_invites)
 ):
     invites.append({"link": str(uuid4()), "time": str(dt.now())})
-    app.state.redis.set(user["preferred_username"], json.dumps(invites))
+    await app.state.redis.set(user["preferred_username"], json.dumps(invites))
+    print(invites, json.dumps(invites))
     return RedirectResponse(request.url_for("home"))
 
 
@@ -160,5 +183,57 @@ async def invite_keycloak_create(
 async def invite_keycloak_delete(
     request: Request, user=Depends(get_user), invites=Depends(get_invites)
 ):
-    invite =request.query_params.get("invite")
-    # TODO
+    invite_to_delete = request.query_params.get("invite")
+    purged = [i for i in invites if i["link"] != invite_to_delete]
+    await app.state.redis.set(user["preferred_username"], json.dumps(purged))
+    return RedirectResponse(request.url_for("home"))
+
+
+@app.get("/register/{invite}")
+async def register_invite(
+    request: Request, invite: str, invites=Depends(get_invites)
+):
+    matching, username = False, None
+    for username in invites:
+        if invite in [x["link"] for x in invites[username]]:
+            matching = True
+            username = username
+
+    if not matching:
+        return RedirectResponse(request.url_for("login"))
+
+    context = {"request": request, "username": username}
+    return templates.TemplateResponse("register.html", context=context)
+
+
+@app.post("/form/keycloak/register")
+def form_keycloak_register(
+    request: Request,
+    first_name: str = Form(...),
+    last_name: str = Form(...),
+    username: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+):
+    user_id = app.state.keycloak.create_user(
+        {
+            "email": email,
+            "username": username,
+            "enabled": True,
+            "firstName": first_name,
+            "lastName": last_name,
+            "credentials": [
+                {
+                    "value": password,
+                    "type": "password",
+                }
+            ],
+            "realmRoles": [
+                "user_default",
+            ],
+        }
+    )
+    app.state.keycloak.send_verify_email(user_id=user_id)
+
+    context = {"request": request, "success": True}
+    return templates.TemplateResponse("register.html", context=context)
